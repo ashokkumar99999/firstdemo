8eb1f9c3f062ad82a458a8561d9807b790a0fdf3
yash1thkumar <yashonethreddy@gmail.com>
2021-08-25 13:40:22 +0530

    "First commit push from YASH-PC"

---
+++ Basic Project/src/GoogleTest/GoogleSite.java
@@ -0,0 +1,11 @@
+package GoogleTest;
+
+public class GoogleSite {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Google site loaded");
+	}
+
+}
